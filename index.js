f5d3587b19ebecf80828aa56371e21f1abcc4367
Angie Newbie <angel890817@hotmail.com>
2025-04-15 15:53:16 +0900

    server run

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const name = prompt('What is your name?')
-console.log('hello world')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello world!')
+})
+
+// start the given server on the given port
+// the call back called when the server run
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
