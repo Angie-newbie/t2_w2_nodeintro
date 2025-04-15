9ee15109b8a376740cc112aa30f3f5ae2922cb86
Angie Newbie <angel890817@hotmail.com>
2025-04-15 16:12:15 +0900

    started route

--- index.js
+++ index.js
@@ -2,8 +2,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const person = {
+    name: 'matt',
+    age: 54
+}
+
+const posts = [
+    {
+        title: 'post 1',
+        body: '',
+        isPublish: false
+    },
+    {
+        title: 'post 2',
+        body: '',
+        isPublish: true
+    }
+]
+
 app.get('/', (req, res) => {
-  res.send('Hello world!')
+    // Serialize person and send to the client
+  res.send(person)
+})
+
+
+
+app.get('/posts', (req,res) => {
+    res.send(posts)
 })
 
 // start the given server on the given port
